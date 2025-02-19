d267c7711a8c606339bbbf72f89ebacd1c3498f9
Riaz Ahmed <riazahmed0147@gmail.com>
2025-02-19 19:43:48 +0500

    fixed links

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,22 @@ function App() {
       <TestimonialCard
         name="Sarah Dole"
         username="sarahdole"
-        profileImg="/src/assets/profile-thumbnail.png" // Place this inside `public/assets/`
+        profileImg="/public/profile-thumbnail.png" // Place this inside `public/assets/`
         content="I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!"
       />
 
       <div className="credits" data-gfe-screenshot-exclude="true">
-        A challenge by
+        A challenge by {' '}
         <a
           href="https://www.greatfrontend.com/projects?ref=challenges"
           target="_blank"
         >
           GreatFrontEnd Projects
-        </a>. Built by
+        </a>. Built by {' '}
         <a
-          href="https://www.greatfrontend.com/projects/u/riaz.ahmed"
+          href="https://www.greatfrontend.com/projects/u/riazahmed0147"
           target="_blank"
-        >
-          Riaz Ahmed
-        </a> .
+        >Riaz Ahmed</a> .
       </div>
     </>
   );
